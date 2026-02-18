60d670e82f5d8b9c24539718d17a25e5774c3a97
Anders Folkesson <anders.folkesson@gu.se>
2026-02-18 16:08:26 +0100

    check if logger should be initialized

--- common.py
+++ common.py
@@ -3,12 +3,10 @@ import os
 from ccipy.utils.cci_logger import CCILogger
 from ccipy.omero.cci_omero_connection import OmeroConnection
 from ccipy.omero.omero_getter_ctx import OmeroGetterCtx
-from ccipy.yolo_utils.vectors_from_geometries import geometries_to_vectors_normalized, save_vectors_to_txt
-from ccipy.omero.omero_colors import omero_rint_to_rgb,omero_rint_to_rgba
+from ccipy.omero.omero_colors import omero_rint_to_rgba
 from ccipy.utils.roi_geometry import RoiGeometry
 from skimage.transform import resize
 from PIL import Image
-import shutil
 import tifffile as tiff
 import numpy as np
 
@@ -25,9 +23,10 @@ OMERO_HOST = "omero-cci-cli.gu.se"
 OMERO_TEST_HOST = "omero-cli.test.gu.se"
 OMERO_PORT = '4064'
 
-def init(session_token: str, session_group: str, use_test_host: bool = False) -> OmeroConnection:
+def init(session_token: str, session_group: str, use_test_host: bool = False, init_log: bool = True) -> OmeroConnection:
 
-    CCILogger.setup_logger("logfile.log", "omero_test")
+    if init_log:
+        CCILogger.setup_logger("logfile.log", "omero_test")
 
     if use_test_host:
         connection = OmeroConnection(OMERO_TEST_HOST,OMERO_PORT,session_token)
@@ -51,7 +50,7 @@ def color_stretch(img, low_pt=1, high_pt=99):
     return stretched
 
 
-def downscale_data(image: Path, target_size: tuple[int, int]):
+def downscale_data(image: Path, target_size: tuple[int, int]) -> Path | None:
     if image.suffix.lower() not in {".tif", ".tiff", ".png", ".jpg", ".jpeg", ".bmp", ".webp"}:
         return None
     img = tiff.imread(image)
@@ -79,9 +78,9 @@ def downscale_data(image: Path, target_size: tuple[int, int]):
         return Path(img_name_png)
     else:
         CCILogger.info(f"Image {image.name} already at target size {target_size}, skipping downscale.")
-    return image.stem
+    return image
 
-def download_downscaled_image(connection: OmeroConnection, img_id: int, images_dir: Path, img_size: int = 512) -> tuple[str, int, int]:
+def download_downscaled_image(connection: OmeroConnection, img_id: int, images_dir: Path, img_size: int = 512) -> tuple[Path | None, int, int]:
     with OmeroGetterCtx(connection) as getter:
         img = connection.get_image(img_id)
         img_width = img.getSizeX()
@@ -90,24 +89,6 @@ def download_downscaled_image(connection: OmeroConnection, img_id: int, images_d
         new_img_name = downscale_data(images_dir / Path(img.getName()), (img_size, img_size))
         
         return new_img_name, img_width, img_height
-
-
-# def download_and_downscale_image(connection: OmeroConnection, img_id, vectors_dir: Path, images_dir: Path, img_size: int = 512):
-
-#     with OmeroGetterCtx(connection) as getter:
-#         img = connection.get_image(img_id)
-#         img_width = img.getSizeX()
-#         img_height = img.getSizeY()
-#         getter.download_original_image_file(img_id, images_dir)
-#         new_img_name = downscale_data(images_dir / Path(img.getName()), (img_size, img_size))
-
-
-# def download_images(connection: OmeroConnection, dataset_ids: list[int], vectors_dir: Path, images_dir: Path, img_size: int = 512):
-
-#     with OmeroGetterCtx(connection) as getter:
-#         for dataset_id in dataset_ids:
-#             for img_id in getter.get_image_ids_from_dataset(dataset_id):
-#                 download_downscaled_image(connection, img_id, images_dir, img_size)
 
 
 def geometry_to_class_definitions(geometry: RoiGeometry) -> int:
